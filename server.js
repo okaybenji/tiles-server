b55c7a0d673b4ae39f163ef7db90499e2d17e168
Benji Kay <okaybenji@gmail.com>
2017-01-22 12:54:04 -0600

    refactor server and prevent clients broadcasting to themselves

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ port: 8100 });
 
-wss.broadcast = (msg) => {
-  const str = JSON.stringify(msg);
-
-  wss.clients.forEach(client => {
-    if (client.readyState !== client.OPEN) {
-      console.log(client.id, 'state is', client.readyState);
-      return;
-    }
-
-    client.send(str);
-  });
-};
-
 wss.on('connection', ws => {
-  console.log('user connected with IP', ws.handshake.address);
+  const ip = ws._socket.remoteAddress;
+  console.log('user connected with IP', ip);
+
+  const broadcast = msg => {
+    wss.clients.forEach(client => {
+      if (client.readyState !== client.OPEN) {
+        console.log(client.id, 'state is', client.readyState);
+        return;
+      }
+
+      // clients shouldn't broadcast to themselves
+      if (client === ws) {
+        return;
+      }
+
+      client.send(msg);
+    });
+  };
 
   ws.on('message', msg => {
-    const actions = {
-      reset() {
-        console.log(ws.handshake.address,'requested reset');
-        ws.broadcast({action: 'reset'});
-      },
-      autoFill() {
-        console.log(ws.handshake.address,'requested auto-fill');
-        ws.broadcast({action: 'autoFill'});
-      },
-      nextColor($index) {
-        ws.broadcast.emit({action: 'nextColor', $index});
-      },
-    };
+    const data = JSON.parse(msg);
+    const actions = ['reset', 'autoFill', 'nextColor'];
+    const actionIsValid = actions.findIndex(action => action === data.action) > -1;
 
-    actions[msg.action]();
+    if (actionIsValid) {
+      broadcast(msg);
+    } else {
+      console.warn(ip, 'requested unknown action:', data.action);
+    }
   });
 });
