fc910d801248cc2ddbb4a950a5511421ebe27bda
Benji Kay <okaybenji@gmail.com>
2017-01-22 12:00:24 -0600

    rewrite server using ws and es6

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
-var io = require('socket.io').listen(8100);
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({ port: 8100 });
 
-io.sockets.on('connection', function(socket) {
-    console.log('user connected with IP', socket.handshake.address);
-    
-    socket.on('requestReset', function() {
-        console.log(socket.handshake.address,'requested reset');
-        socket.broadcast.emit('reset');
-    });
-    
-    socket.on('requestAutoFill', function() {
-        console.log(socket.handshake.address,'requested auto-fill');
-        socket.broadcast.emit('autoFill');
-    });
-    
-    socket.on('requestNextColor', function($index) {
-        socket.broadcast.emit('nextColor', $index);
-    });
+wss.broadcast = (msg) => {
+  const str = JSON.stringify(msg);
+
+  wss.clients.forEach(client => {
+    if (client.readyState !== client.OPEN) {
+      console.log(client.id, 'state is', client.readyState);
+      return;
+    }
+
+    client.send(str);
+  });
+};
+
+wss.on('connection', ws => {
+  console.log('user connected with IP', ws.handshake.address);
+
+  ws.on('message', msg => {
+    const actions = {
+      reset() {
+        console.log(ws.handshake.address,'requested reset');
+        ws.broadcast({action: 'reset'});
+      },
+      autoFill() {
+        console.log(ws.handshake.address,'requested auto-fill');
+        ws.broadcast({action: 'autoFill'});
+      },
+      nextColor($index) {
+        ws.broadcast.emit({action: 'nextColor', $index});
+      },
+    };
+
+    actions[msg.action]();
+  });
 });
